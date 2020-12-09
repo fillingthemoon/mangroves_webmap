cbeb58f48c5605d08fb35e214c88b058a1b4ace0
philemonheng <phil.hsy@outlook.com>
2020-12-10 07:26:22 +0800

    separate html, css, and js

---
+++ main.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmlsbGluZ3RoZW1vb24iLCJhIjoiY2tpaGxrbjZmMDNicTJ4bThmd3preWJvbyJ9.ca_X_SaaPktyuEBL2RzoRA';
+
+var map = new mapboxgl.Map({
+  container: 'map', // container id
+  style: 'mapbox://styles/mapbox/streets-v8', // style URL
+  center: [103.95, 1.377856], // starting position [lng, lat]
+  zoom: 16 // starting zoom
+});
