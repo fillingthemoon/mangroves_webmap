9938f4bdc584647308094d250d5f047d09503b54
philemonheng <phil.hsy@outlook.com>
2020-12-10 21:08:00 +0800

    update code

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZmlsbGluZ3RoZW1vb24iLCJhIjoiY2tpaGxrbjZmMDNic
 var map = new mapboxgl.Map({
   container: 'map', // container id
   style: 'mapbox://styles/mapbox/dark-v9', // style URL
-  center: [103.95, 1.377856], // starting position [lng, lat]
-  zoom: 16 // starting zoom
+  center: [103.9515, 1.3782], // starting position [lng, lat]
+  zoom: 17 // starting zoom
 });
 
 
