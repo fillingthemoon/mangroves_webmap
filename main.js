b1cd18742bdb1d2ae868c41f2939c7599742a386
philemonheng <phil.hsy@outlook.com>
2020-12-11 23:12:20 +0800

    renamed geojsons

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ var map = new mapboxgl.Map({
 });
 
 map.on('load', function () {
-  // load in new mangroves
-  map.addsource('new_mgvs', {
+  // Load in new mangroves
+  map.addSource('new_mgvs', {
     'type': 'geojson',
     'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/new_mgvs.json'
   });
-  map.addlayer({
-    'id': 'new mangroves',
+  map.addLayer({
+    'id': 'New mangroves',
     'type': 'fill',
     'source': 'new_mgvs',
     'layout': {
@@ -27,7 +27,7 @@ map.on('load', function () {
     }
   });
 
-  // Load in old mangroves
+  // Load in new mangroves
   map.addSource('old_mgvs', {
     'type': 'geojson',
     'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/old_mgvs.json'
@@ -41,18 +41,18 @@ map.on('load', function () {
       'visibility': 'visible'
     },
     'paint': {
-      'fill-color': '#088',
+      'fill-color': '#008888',
       'fill-opacity': 0.3
     }
   });
 
   // load in new plots
-  map.addsource('new_plots', {
+  map.addSource('new_plots', {
     'type': 'geojson',
-    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/new_plots.json'
+    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/new_plots_rdm.json'
   });
-  map.addlayer({
-    'id': 'New plots',
+  map.addLayer({
+    'id': 'Plots for new mangroves',
     'type': 'fill',
     'source': 'new_plots',
     'layout': {
@@ -60,18 +60,18 @@ map.on('load', function () {
       'visibility': 'visible'
     },
     'paint': {
-      'fill-color': '#ed5826',
-      'fill-opacity': 0.3
+      'fill-color': '#632a18',
+      'fill-opacity': 1
     }
   });
 
   // Load in old plots
   map.addSource('old_plots', {
     'type': 'geojson',
-    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/old_plots.json'
+    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/old_plots_rdm.json'
   });
   map.addLayer({
-    'id': 'Old plots',
+    'id': 'Plots for old mangroves',
     'type': 'fill',
     'source': 'old_plots',
     'layout': {
@@ -79,16 +79,16 @@ map.on('load', function () {
       'visibility': 'visible'
     },
     'paint': {
-      'fill-color': '#088',
-      'fill-opacity': 0.3
+      'fill-color': '#013636',
+      'fill-opacity': 1
     }
   });
 });
 
 
 // enumerate ids of the layers
-var toggleableLayerIds = ['New mangroves', 'Old mangroves'];
-var colors = ['#ED5826', '#088']
+var toggleableLayerIds = ['New mangroves', 'Plots for new mangroves', 'Old mangroves', 'Plots for old mangroves'];
+var colors = ['#ED5826', '#632a18', '#088', '#035454']
 
 // set up the corresponding toggle button for each layer
 for (var i = 0; i < toggleableLayerIds.length; i++) {
