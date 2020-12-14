4ad3cf4f5b281b76bd5dd863101064f36351b764
philemonheng <phil.hsy@outlook.com>
2020-12-15 01:05:17 +0800

    fix swiper

--- main.js
+++ main.js
@@ -23,320 +23,208 @@ var map = new mapboxgl.Compare(beforeMap, afterMap, container, {
 });
 
 // Add GeoJSON files
-map.on('load', function () {
-  // Load in new mangroves
-  map.addSource('new_mgvs', {
-    'type': 'geojson',
-    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/new_mgvs.json'
-  });
-  map.addLayer({
-    'id': 'new_mgvs',
-    'type': 'fill',
-    'source': 'new_mgvs',
-    'layout': {
-      'visibility': 'visible' // make layer visible by default
-    },
-    'paint': {
-      'fill-color': '#008888',
-      'fill-opacity': 0.2
+
+baMapArr = [beforeMap];
+for (baMap of baMapArr) {
+  baMap.on('load', function () {
+    // Load in new and old mangrove polygons
+    no_mgvs_arr = ["new_mgvs", "old_mgvs"]
+    for (no_mgvs of no_mgvs_arr) {
+      color = no_mgvs == "new_mgvs" ? '#008888' : '#ed5826';
+      baMap.addSource(no_mgvs, {
+        'type': 'geojson',
+        'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/' + no_mgvs + '.json'
+      });
+      baMap.addLayer({
+        'id': no_mgvs,
+        'type': 'fill',
+        'source': no_mgvs,
+        'layout': {
+          'visibility': 'visible' // make layer visible by default
+        },
+        'paint': {
+          'fill-color': color,
+          'fill-opacity': 0.2
+        }
+      });
     }
-  });
 
-  // Load in new mangroves
-  map.addSource('old_mgvs', {
-    'type': 'geojson',
-    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/old_mgvs.json'
-  });
-  map.addLayer({
-    'id': 'old_mgvs',
-    'type': 'fill',
-    'source': 'old_mgvs',
-    'layout': {
-      'visibility': 'visible' // make layer visible by default
-    },
-    'paint': {
-      'fill-color': '#ed5826',
-      'fill-opacity': 0.2
+    // Load in new and old mangrove plots
+    plots = ["buff_n1", "buff_n2", "buff_n3", "buff_n4", "buff_o1", "buff_o2", "buff_o3", "buff_o4"];
+    for (i in plots) {
+      color = plots[i].includes("_n") ? "#036569" : "#8f470d";
+      baMap.addSource(plots[i], {
+        'type': 'geojson',
+        'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/' + plots[i] + '.json'
+      });
+      baMap.addLayer({
+        'id': plots[i],
+        'type': 'fill',
+        'source': plots[i],
+        'layout': {
+          'visibility': 'visible' // make layer visible by default
+        },
+        'paint': {
+          'fill-color': color,
+          'fill-opacity': 1
+        }
+      });
     }
-  });
 
-  // load in new plots
-  map.addSource('buff_n1', {
-    'type': 'geojson',
-    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/buff_n1.json'
-  });
-  map.addLayer({
-    'id': 'buff_n1',
-    'type': 'fill',
-    'source': 'buff_n1',
-    'layout': {
-      'visibility': 'visible' // make layer visible by default
-    },
-    'paint': {
-      'fill-color': '#036569',
-      'fill-opacity': 1
-    }
-  });
+    html_popup_txt = [
+      "<b>Plot ID:</b> N1 <br><br>"
+      + "<b>Mangrove species:</b><br> - A <br> - B <br> - C <br><br>"
+      + "<b>Total carbon stock:</b> X",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h"
+    ];
 
-  map.addSource('buff_n2', {
-    'type': 'geojson',
-    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/buff_n2.json'
-  });
-  map.addLayer({
-    'id': 'buff_n2',
-    'type': 'fill',
-    'source': 'buff_n2',
-    'layout': {
-      'visibility': 'visible' // make layer visible by default
-    },
-    'paint': {
-      'fill-color': '#036569',
-      'fill-opacity': 1
-    }
-  });
+    // Popup for points
+    baMap.on('click', plots[0], function (e) {
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(html_popup_txt[0])
+        .addTo(baMap);
+    });
+    // Change the cursor to a pointer when the mouse is over the states layer.
+    baMap.on('mouseenter', plots[0], function () {
+      baMap.getCanvas().style.cursor = 'pointer';
+    });
+    // Change it back to a pointer when it leaves.
+    baMap.on('mouseleave', plots[0], function () {
+      baMap.getCanvas().style.cursor = '';
+    });
 
-  map.addSource('buff_n3', {
-    'type': 'geojson',
-    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/buff_n3.json'
-  });
-  map.addLayer({
-    'id': 'buff_n3',
-    'type': 'fill',
-    'source': 'buff_n3',
-    'layout': {
-      'visibility': 'visible' // make layer visible by default
-    },
-    'paint': {
-      'fill-color': '#036569',
-      'fill-opacity': 1
-    }
-  });
+    baMap.on('click', plots[1], function (e) {
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(html_popup_txt[1])
+        .addTo(baMap);
+    });
+    // Change the cursor to a pointer when the mouse is over the states layer.
+    baMap.on('mouseenter', plots[1], function () {
+      baMap.getCanvas().style.cursor = 'pointer';
+    });
+    // Change it back to a pointer when it leaves.
+    baMap.on('mouseleave', plots[1], function () {
+      baMap.getCanvas().style.cursor = '';
+    });
 
-  map.addSource('buff_n4', {
-    'type': 'geojson',
-    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/buff_n4.json'
-  });
-  map.addLayer({
-    'id': 'buff_n4',
-    'type': 'fill',
-    'source': 'buff_n4',
-    'layout': {
-      'visibility': 'visible' // make layer visible by default
-    },
-    'paint': {
-      'fill-color': '#036569',
-      'fill-opacity': 1
-    }
-  });
+    baMap.on('click', plots[2], function (e) {
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(html_popup_txt[2])
+        .addTo(baMap);
+    });
+    // Change the cursor to a pointer when the mouse is over the states layer.
+    baMap.on('mouseenter', plots[2], function () {
+      baMap.getCanvas().style.cursor = 'pointer';
+    });
+    // Change it back to a pointer when it leaves.
+    baMap.on('mouseleave', plots[2], function () {
+      baMap.getCanvas().style.cursor = '';
+    });
 
-  // Load in old plots
-  map.addSource('buff_o1', {
-    'type': 'geojson',
-    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/buff_o1.json'
-  });
-  map.addLayer({
-    'id': 'buff_o1',
-    'type': 'fill',
-    'source': 'buff_o1',
-   'layout': {
-      'visibility': 'visible' // make layer visible by default
-    },
-    'paint': {
-      'fill-color': '#8f470d',
-      'fill-opacity': 1
-    }
-  });
+    baMap.on('click', plots[3], function (e) {
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(html_popup_txt[3])
+        .addTo(baMap);
+    });
+    // Change the cursor to a pointer when the mouse is over the states layer.
+    baMap.on('mouseenter', plots[3], function () {
+      baMap.getCanvas().style.cursor = 'pointer';
+    });
+    // Change it back to a pointer when it leaves.
+    baMap.on('mouseleave', plots[3], function () {
+      baMap.getCanvas().style.cursor = '';
+    });
 
-  map.addSource('buff_o2', {
-    'type': 'geojson',
-    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/buff_o2.json'
-  });
-  beforemap.addLayer({
-    'id': 'buff_o2',
-    'type': 'fill',
-    'source': 'buff_o2',
-   'layout': {
-      'visibility': 'visible' // make layer visible by default
-    },
-    'paint': {
-      'fill-color': '#8f470d',
-      'fill-opacity': 1
-    }
-  });
+    baMap.on('click', plots[4], function (e) {
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(html_popup_txt[4])
+        .addTo(baMap);
+    });
+    // Change the cursor to a pointer when the mouse is over the states layer.
+    baMap.on('mouseenter', plots[4], function () {
+      baMap.getCanvas().style.cursor = 'pointer';
+    });
+    // Change it back to a pointer when it leaves.
+    baMap.on('mouseleave', plots[4], function () {
+      baMap.getCanvas().style.cursor = '';
+    });
 
-  map.addSource('buff_o3', {
-    'type': 'geojson',
-    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/buff_o3.json'
+    baMap.on('click', plots[5], function (e) {
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(html_popup_txt[5])
+        .addTo(baMap);
+    });
+    // Change the cursor to a pointer when the mouse is over the states layer.
+    baMap.on('mouseenter', plots[5], function () {
+      baMap.getCanvas().style.cursor = 'pointer';
+    });
+    // Change it back to a pointer when it leaves.
+    baMap.on('mouseleave', plots[5], function () {
+      baMap.getCanvas().style.cursor = '';
+    });
+
+    baMap.on('click', plots[6], function (e) {
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(html_popup_txt[6])
+        .addTo(baMap);
+    });
+    // Change the cursor to a pointer when the mouse is over the states layer.
+    baMap.on('mouseenter', plots[6], function () {
+      baMap.getCanvas().style.cursor = 'pointer';
+    });
+    // Change it back to a pointer when it leaves.
+    baMap.on('mouseleave', plots[6], function () {
+      baMap.getCanvas().style.cursor = '';
+    });
+
+    baMap.on('click', plots[7], function (e) {
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(html_popup_txt[7])
+        .addTo(baMap);
+    });
+    // Change the cursor to a pointer when the mouse is over the states layer.
+    baMap.on('mouseenter', plots[7], function () {
+      baMap.getCanvas().style.cursor = 'pointer';
+    });
+    // Change it back to a pointer when it leaves.
+    baMap.on('mouseleave', plots[7], function () {
+      baMap.getCanvas().style.cursor = '';
+    });
+
+    baMap.on('click', plots[8], function (e) {
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(html_popup_txt[8])
+        .addTo(baMap);
+    });
+    // Change the cursor to a pointer when the mouse is over the states layer.
+    baMap.on('mouseenter', plots[8], function () {
+      baMap.getCanvas().style.cursor = 'pointer';
+    });
+    // Change it back to a pointer when it leaves.
+    baMap.on('mouseleave', plots[8], function () {
+      baMap.getCanvas().style.cursor = '';
+    });
+
+    baMap.addControl(new mapboxgl.ScaleControl({position: 'bottom-right'}));
   });
-  map.addLayer({
-    'id': 'buff_o3',
-    'type': 'fill',
-    'source': 'buff_o3',
-   'layout': {
-      'visibility': 'visible' // make layer visible by default
-    },
-    'paint': {
-      'fill-color': '#8f470d',
-      'fill-opacity': 1
-    }
-  });
+}
 
-  map.addSource('buff_o4', {
-    'type': 'geojson',
-    'data': 'https://fillingthemoon.github.io/mangroves_webmap/geojsons/buff_o4.json'
-  });
-  map.addLayer({
-    'id': 'buff_o4',
-    'type': 'fill',
-    'source': 'buff_o4',
-   'layout': {
-      'visibility': 'visible' // make layer visible by default
-    },
-    'paint': {
-      'fill-color': '#8f470d',
-      'fill-opacity': 1
-    }
-  });
-
-
-  // Popup for points
-  map.on('click', 'buff_n1', function (e) {
-    new mapboxgl.Popup()
-      .setLngLat(e.lngLat)
-      .setHTML(
-        "<b>Plot ID:</b> N1 <br><br>"
-        + "<b>Mangrove species:</b><br> - A <br> - B <br> - C <br><br>"
-        + "<b>Total carbon stock:</b> X"
-      )
-      .addTo(map);
-  });
-
-  // Change the cursor to a pointer when the mouse is over the states layer.
-  map.on('mouseenter', 'buff_n1', function () {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-
-  // Change it back to a pointer when it leaves.
-  map.on('mouseleave', 'buff_n1', function () {
-    map.getCanvas().style.cursor = '';
-  });
-
-  map.on('click', 'buff_n2', function (e) {
-    new mapboxgl.Popup()
-      .setLngLat(e.lngLat)
-      .setHTML("id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis")
-      .addTo(map);
-  });
-
-  // Change the cursor to a pointer when the mouse is over the states layer.
-  map.on('mouseenter', 'buff_n2', function () {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-
-  // Change it back to a pointer when it leaves.
-  map.on('mouseleave', 'buff_n2', function () {
-    map.getCanvas().style.cursor = '';
-  });
-
-  map.on('click', 'buff_n3', function (e) {
-    new mapboxgl.Popup()
-      .setLngLat(e.lngLat)
-      .setHTML("id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis")
-      .addTo(map);
-  });
-
-  // Change the cursor to a pointer when the mouse is over the states layer.
-  map.on('mouseenter', 'buff_n3', function () {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-
-  // Change it back to a pointer when it leaves.
-  map.on('mouseleave', 'buff_n3', function () {
-    map.getCanvas().style.cursor = '';
-  });
-
-  map.on('click', 'buff_n4', function (e) {
-    new mapboxgl.Popup()
-      .setLngLat(e.lngLat)
-      .setHTML("id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis")
-      .addTo(map);
-  });
-
-  // Change the cursor to a pointer when the mouse is over the states layer.
-  map.on('mouseenter', 'buff_n4', function () {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-
-  // Change it back to a pointer when it leaves.
-  map.on('mouseleave', 'buff_n4', function () {
-    map.getCanvas().style.cursor = '';
-  });
-
-  map.on('click', 'buff_o1', function (e) {
-    new mapboxgl.Popup()
-      .setLngLat(e.lngLat)
-      .setHTML("id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis")
-      .addTo(map);
-  });
-
-  map.on('mouseenter', 'buff_o1', function () {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-
-  map.on('mouseleave', 'buff_o1', function () {
-    map.getCanvas().style.cursor = '';
-  });
-
-  map.on('click', 'buff_o2', function (e) {
-    new mapboxgl.Popup()
-      .setLngLat(e.lngLat)
-      .setHTML("id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis")
-      .addTo(map);
-  });
-
-  map.on('mouseenter', 'buff_o2', function () {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-
-  map.on('mouseleave', 'buff_o2', function () {
-    map.getCanvas().style.cursor = '';
-  });
-
-  map.on('click', 'buff_o3', function (e) {
-    new mapboxgl.Popup()
-      .setLngLat(e.lngLat)
-      .setHTML("id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis")
-      .addTo(map);
-  });
-
-  map.on('mouseenter', 'buff_o3', function () {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-
-  map.on('mouseleave', 'buff_o3', function () {
-    map.getCanvas().style.cursor = '';
-  });
-
-  map.on('click', 'buff_o4', function (e) {
-    new mapboxgl.Popup()
-      .setLngLat(e.lngLat)
-      .setHTML("id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis")
-      .addTo(map);
-  });
-
-  map.on('mouseenter', 'buff_o4', function () {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-
-  map.on('mouseleave', 'buff_o4', function () {
-    map.getCanvas().style.cursor = '';
-  });
-
-  map.addControl(new mapboxgl.ScaleControl({position: 'bottom-right'}));
-});
-
-
-// enumerate ids of the layers
+// Legend
 var layers = ['New mangrove area', 'Plots (new mangroves)', 'Old mangrove area', 'Plots (old mangroves)'];
 var colors = ['#ed5826', '#8f470d', '#008888', '#036569']
 
